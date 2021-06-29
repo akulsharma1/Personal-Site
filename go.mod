70364f36104a589afcc5fbee8b764189dd148684
akulsharma1 <akulsharma02@gmail.com>
2021-06-29 13:08:12 -0700

    Co-authored-by: yummy <yummy05@users.noreply.github.com>

--- go.mod
+++
@@ -1,9 +0,0 @@
-module personalsite
-
-go 1.16
-
-require (
-	github.com/labstack/echo v3.3.10+incompatible // indirect
-	github.com/labstack/gommon v0.3.0 // indirect
-	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
-)
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/',(req,res) => {
+
+    res.sendFile(__dirname+'\\view\\homepage.html')
+    return
+})
+
+
+
+app.get('/*',(req,res) => {
+
+    res.status(404).sendFile(__dirname+'\\view\\404.html')
+    return
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`app is listening on port ${port}`)
+})
